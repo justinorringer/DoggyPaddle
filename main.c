da69455b7ad07f09e8efbddf9ef70641d5aeb082
Daniel Agbay <dagbay@redhat.com>
2022-12-06 18:43:17 -0600

    makefile and initial game loop structure

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <gb/gb.h>
+
+void init();
+
+void main() {
+    init();
+
+    while(1){
+        
+        // do game
+    }
+}
+
+void init(){
+    DISPLAY_ON;
+    SHOW_BKG; // HIDE_BKG to hide
+
+}
