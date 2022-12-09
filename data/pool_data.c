53b4ea82e869064331d5320c41d54c4fddeefa81
Daniel Agbay <dagbay@redhat.com>
2022-12-09 13:49:36 -0600

    use const

--- data/pool_data.c
+++ data/pool_data.c
@@ -1,6 +1,6 @@
 #include <gb/gb.h>
 #include "pool_data.h"
 
-EnemyData enemy_data[1] = {
+EnemyData enemy_data[ENEMY_DATA_COUNT] = {
     { { 10, 0, 0, 0 }, 300, 100, 0, 1 },
 };
--- data/pool_data.h
+++ data/pool_data.h
@@ -5,7 +5,7 @@
 
 #define ENEMY_DATA_COUNT (UINT8)1
 
-typedef struct {
+typedef struct EnemyData{
     UBYTE sprite_id[4];
     UINT16 x; // in tiles so it doesn't overflow
     UINT8 y; // in tiles so it doesn't overflow
@@ -13,6 +13,6 @@ typedef struct {
     UINT8 movement_type;
 } EnemyData;
 
-extern EnemyData enemy_data[1];
+extern EnemyData enemy_data[ENEMY_DATA_COUNT];
 
 #endif
